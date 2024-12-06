ab430b5be9ca40607cc7b4b898552d3ceea167ed
Yakov <jacob.work477@gmail.com>
2024-12-06 16:46:25 +0300

    delete unused DTO

--- testtask/src/main/kotlin/com/example/testtask/dto/Error.kt
+++
@@ -1,7 +0,0 @@
-package com.example.testtask.dto
-
-enum class Error(
-    val errorText: String
-) {
-    MISSING_FIELD(errorText = "Request body deserialize error: missing field `id` at line 5 column 1")
-}
